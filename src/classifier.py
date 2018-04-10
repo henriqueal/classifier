d7dbe993fbf89433a70c92eaf4640bb2cf80858c
henriqueal <henriquebrmg@gmail.com>
2018-04-09 21:19:58 -0300

    z-score implemented

--- src/classifier.py
+++ src/classifier.py
@@ -7,7 +7,7 @@ def k_nn(train_data, test_data, k_of_knn, normalization):
 	#mat : rows = test_data.len; cols = train_data.len 
 	mat_of_distances = euclidean_distance(train_data, test_data)
 
-   	mat_of_k_classes_nearest = k_nearest_classes(mat_of_distances)
+   	mat_of_k_classes_nearest = k_nearest_classes(mat_of_distances, k)
 
 	pair_classified = get_most_recurrence_class(mat_of_k_classes_nearest)
    	
--- src/normalizer.py
+++ src/normalizer.py
@@ -1,5 +1,17 @@
+def mean(data):
+	print("deve retornar a media dos dados")
+	return 1
+
+def standard_deviation(data):
+	print("deve retornar o desvio padrao dos dados")
+	return 1
+
 def z_score(data):
-	print("z_score: Deve retornar o vetor normalizado")
+	mean = mean(data)
+	dev = standard_deviation(data)
+	
+	for i range(len(data)):
+		data[i] = (data[i] - mean)/dev
 	return data
 
 z_score(range(2))
