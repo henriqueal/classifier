ff91600099f17a88e0b45db01a77d030b5d3d05c
dpasquini1 <diego.pasquini@neppoadm.com.br>
2018-04-16 23:25:44 -0300

    making normalization boolean, fix string to int

--- src/main.py
+++ src/main.py
@@ -8,6 +8,12 @@ filename = "../input/dados.txt"
 
 #Ler do usuario
 normalization = raw_input("Deseja utilizar o z-score? (y/n)")
+
+if(normalization == "y"):
+    normalization = True
+else:
+    normalization = False 
+
 k_of_kfold = raw_input("Por favor informe o numero de particoes do k-fold:")
 k_of_knn = raw_input("Por favor informe o numero de vizinhos do k-NN:")
 filename = raw_input("Por favor informe o nome do arquivo de dados:")
@@ -20,8 +26,8 @@ numberAttributes = 0
 with open('../input/'+filename) as f:
     for line in f:
         if(line_count == 0):
-            numberSamples = line.split(' ')[0]
-            numberAttributes = line.split(' ')[1]
+            numberSamples = int(line.split(' ')[0])
+            numberAttributes = int(line.split(' ')[1])
             line_count = line_count + 1
         else:
             matrix_line = line.split(' ')
