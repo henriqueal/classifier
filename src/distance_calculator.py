7e24c9a8a0b66c2e26c2c507d4a2ea2b7a5bcef7
henriqueal <henriquebrmg@gmail.com>
2018-04-21 18:44:38 -0300

    alter rows to columns

--- src/distance_calculator.py
+++ src/distance_calculator.py
@@ -1,39 +1,35 @@
 import math
 
 def euclidean_distance(train_data, test_data):
-	#print("Deve retornar a matriz de distancia entre o vetor de teste e o de treinamento");
-	#print("Quantidade de linhas: tamanho do vetor de teste");
-	#print("Quantidade de colunas: tamanho do vetor de treinamento");
-
 	numberColumns = len(train_data[0]) # Calculando o numero de atributos da amostra
 	result = []
 	aux = []
 	sum = 0.0
-	position = 0
+	position_train = 0
 	position_test = 0
 	position_column = 0
 
-	while(position < len(train_data)):
-		#print("Position:"+str(position))
-		while(position_test < len(test_data)):
+	while(position_test < len(test_data)):
+		#print("Test: "+str(position_test))
+		while(position_train < len(train_data)):
+			#print("  Train: " + str(position_train))
 			while(position_column < numberColumns):
-				sum = sum + (train_data[position][position_column] - test_data[position_test][position_column])**2
-				#print("["+str(position)+"]["+str(position_column)+"]"+" ["+str(position_test)+"]["+str(position_column)+"]")
+				#print("    Coluna: " + str(position_column))
+				sum = sum + (train_data[position_train][position_column] - test_data[position_test][position_column])**2
 				position_column = position_column + 1
-			#print(math.sqrt(sum))
 			aux.append(math.sqrt(sum))
 			sum = 0.0
-			position_test = position_test + 1
+			position_train = position_train + 1
 			position_column = 0
 
 		result.append(aux)
 		aux = []
-		position = position + 1
-		position_test = 0
+		position_test = position_test + 1
+		position_train = 0
 
 	return result
 
 
 
 
-##print(euclidean_distance([[0,1,3],[2,4,5],[6,7,10]], [[11,12,13],[20,21,23],[40,50,60]]))
+print(euclidean_distance([[0,1,3],[2,4,5],[6,7,10]], [[40,50,60]]))
